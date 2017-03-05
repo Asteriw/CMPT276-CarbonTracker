d0f0a2ddb0438850d6ebd28687d03780afedc1b9
Paul <allanpaulw@gmail.com>
2017-03-04 18:41:59 -0800

    Test something out with the database to make sure it's queriable.

--- CarbonFootprintTracker/app/src/main/java/com/cmpt276/kenneyw/carbonfootprinttracker/SelectTransportation.java
+++ CarbonFootprintTracker/app/src/main/java/com/cmpt276/kenneyw/carbonfootprinttracker/SelectTransportation.java
@@ -3,6 +3,8 @@ package com.cmpt276.kenneyw.carbonfootprinttracker;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,6 +12,7 @@ import android.view.View;
 import android.widget.Button;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -59,49 +62,61 @@ public class SelectTransportation extends AppCompatActivity {
     }
 
     private void setupDatabase() {
+        SQLiteDatabase checkDB = null;
         InputStream stream = getResources().openRawResource(R.raw.vehicles);
         BufferedReader reader = new BufferedReader(
                 new InputStreamReader(stream, Charset.forName("UTF-8"))
         );
         String line = "";
-        try {
-            //Step over headers
-            reader.readLine();
-            while ((line = reader.readLine()) != null) {
-                //Split by ','
-                String[] tokens = line.split(",");
-                Log.i("CarbonFootprintTracker", ""+tokens[0] + " ");
-                long newID = database.insertRow(
-                        Integer.parseInt(tokens[0]),
-                        tokens[1],
-                        tokens[2],
-                        Float.parseFloat(tokens[3]),
-                        Float.parseFloat(tokens[4]),
-                        Float.parseFloat(tokens[5]),
-                        Float.parseFloat(tokens[6]),
-                        Float.parseFloat(tokens[7]),
-                        Float.parseFloat(tokens[8]),
-                        Float.parseFloat(tokens[9]),
-                        Float.parseFloat(tokens[10]),
-                        Float.parseFloat(tokens[11]),
-                        Integer.parseInt(tokens[12]),
-                        Float.parseFloat(tokens[13]),
-                        Integer.parseInt(tokens[14]),
-                        Float.parseFloat(tokens[15]),
-                        tokens[16],
-                        tokens[17],
-                        tokens[18],
-                        tokens[19],
-                        Integer.parseInt(tokens[20]),
-                        tokens[21],
-                        tokens[22],
-                        tokens[23]
-                );
+        File path = getDatabasePath("CarDB");
+        if (path.exists()){
+            Log.i("CarbonFootprintTracker", "File exists!");
+        } else {
+            /*try {
+                checkDB = SQLiteDatabase.openDatabase(DB_FULL_PATH, null,
+                        SQLiteDatabase.OPEN_READONLY);
+                checkDB.close();
+            } catch (SQLiteException e) {*/
+                // database doesn't exist yet.
+            try {
+                //Step over headers
+                reader.readLine();
+                while ((line = reader.readLine()) != null) {
+                    //Split by ','
+                    String[] tokens = line.split(",");
+                    Log.i("CarbonFootprintTracker", "" + tokens[0] + " ");
+                    long newID = database.insertRow(
+                            Integer.parseInt(tokens[0]),
+                            tokens[1],
+                            tokens[2],
+                            Float.parseFloat(tokens[3]),
+                            Float.parseFloat(tokens[4]),
+                            Float.parseFloat(tokens[5]),
+                            Float.parseFloat(tokens[6]),
+                            Float.parseFloat(tokens[7]),
+                            Float.parseFloat(tokens[8]),
+                            Float.parseFloat(tokens[9]),
+                            Float.parseFloat(tokens[10]),
+                            Float.parseFloat(tokens[11]),
+                            Integer.parseInt(tokens[12]),
+                            Float.parseFloat(tokens[13]),
+                            Integer.parseInt(tokens[14]),
+                            Float.parseFloat(tokens[15]),
+                            tokens[16],
+                            tokens[17],
+                            tokens[18],
+                            tokens[19],
+                            Integer.parseInt(tokens[20]),
+                            tokens[21],
+                            tokens[22],
+                            tokens[23]
+                    );
+                }
+                Log.i("CarbonFootprintReader", "Created database");
+            } catch (IOException e) {
+                Log.wtf("CarbonFootprintReader", "Error reading vehicles database on line " + line, e);
+                e.printStackTrace();
             }
-            Log.i("CarbonFootprintReader", "Created database");
-        } catch (IOException e){
-            Log.wtf("CarbonFootprintReader", "Error reading vehicles database on line " + line, e);
-            e.printStackTrace();
         }
     }
 
