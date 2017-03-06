bbc642c54c6686854bf203679937cd270412b498
kenneyw <kenneyw@sfu.ca>
2017-03-05 21:03:40 -0800

    Added image to the splash screen. Added an animation. Added authors.

--- CarbonFootprintTracker/app/src/main/java/com/cmpt276/kenneyw/carbonfootprinttracker/SplashScreen.java
+++ CarbonFootprintTracker/app/src/main/java/com/cmpt276/kenneyw/carbonfootprinttracker/SplashScreen.java
@@ -4,7 +4,11 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.TranslateAnimation;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -14,6 +18,16 @@ public class SplashScreen extends AppCompatActivity {
         setContentView(R.layout.activity_splash_screen);
 
         setupStartButton();
+        setupAnimation();
+    }
+
+    private void setupAnimation() {
+        ImageView car = (ImageView) findViewById(R.id.splashcar);
+        TranslateAnimation slide = new TranslateAnimation(-400f, 1400f, -20f, -20f);
+        slide.setInterpolator(new LinearInterpolator());
+        slide.setDuration(3500);
+        slide.setRepeatCount(Animation.INFINITE);
+        car.startAnimation(slide);
     }
 
     private void setupStartButton() {
