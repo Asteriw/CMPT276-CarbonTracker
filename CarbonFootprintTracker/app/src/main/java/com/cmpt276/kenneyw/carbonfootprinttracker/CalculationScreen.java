d230e6f5b042b7d55cb2a8ae3cff00beee6d1ad9
Daniel Lee <dlee0528@hotmail.com>
2017-03-07 22:03:42 -0800

    Display carbon calculation result on CalculationScreen activity

--- CarbonFootprintTracker/app/src/main/java/com/cmpt276/kenneyw/carbonfootprinttracker/CalculationScreen.java
+++ CarbonFootprintTracker/app/src/main/java/com/cmpt276/kenneyw/carbonfootprinttracker/CalculationScreen.java
@@ -1,9 +1,17 @@
 package com.cmpt276.kenneyw.carbonfootprinttracker;
 
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatDialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
 import java.math.RoundingMode;
 import java.text.DecimalFormat;
 
@@ -26,16 +34,54 @@ import java.text.DecimalFormat;
 *  Negative values are not allowed.
 * */
 
-public class CalculationScreen extends AppCompatActivity {
-    private static DecimalFormat df2 = new DecimalFormat(".##"); // for decimal precision
-    final private static double gasoline_CO2_emission_kg_per_gal = 8.89;
-    final private static double diesel_CO2_emission_kg_per_gal = 10.16;
+/*
+public class CalculationScreen extends AppCompatDialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState){
+        // Create the view to show
+        View v = LayoutInflater.from(getActivity())
+                .inflate(R.layout.activity_calculation_screen, null);
 
+        // Create a button listener
+        DialogInterface.OnClickListener listner = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which){
+                    case DialogInterface.BUTTON_POSITIVE:
+                        TextView calculation_result = (TextView) getActivity().findViewById(R.id.calculation_result);
+                        calculation_result.setText("Temporary");
+                        break;
+
+                    case DialogInterface.BUTTON_NEGATIVE:
+                    //Don't do anything just end.
+                    break;
+                }
+            }
+        };
+
+        // Build the alert dialog
+        return new AlertDialog.Builder(getActivity())
+                .setTitle("CO2 Emission Calculation Result")
+                .setView(v)
+                .setPositiveButton(android.R.string.ok, listner)
+                .setNegativeButton(android.R.string.cancel, listner)
+                .create();
+
+    }
+*/
+
+public class CalculationScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculation_screen);
+        TextView calculationResult = (TextView) findViewById(R.id.calculation_result);
+        calculationResult.setText("Temporary");
     }
+
+    private static DecimalFormat df2 = new DecimalFormat(".##"); // for decimal precision
+    final private static double gasoline_CO2_emission_kg_per_gal = 8.89;
+    final private static double diesel_CO2_emission_kg_per_gal = 10.16;
 
     /*Convert CO2 Emission from gallons to kilograms*/
     public double calculate_C02_Emission_of_Diesel_from_gallon_to_kg(double var_in_gal){
@@ -74,4 +120,5 @@ public class CalculationScreen extends AppCompatActivity {
     public static Intent makeIntent(Context context) {
         return new Intent(context, CalculationScreen.class);
     }
+
 }
