011c9698ae8053cf5276cee99f066166df88863e
kenneyw <kenneyw@sfu.ca>
2017-03-08 17:31:33 -0800

    Not sure what this adds anymore, since my merges got jumbled v2

--- CarbonFootprintTracker/app/src/main/java/com/cmpt276/kenneyw/carbonfootprinttracker/EditRouteFragment.java
+++ CarbonFootprintTracker/app/src/main/java/com/cmpt276/kenneyw/carbonfootprinttracker/EditRouteFragment.java
@@ -5,68 +5,74 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatDialogFragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
 
 public class EditRouteFragment extends AppCompatDialogFragment {
+
+
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
 
+        final int pos=getArguments().getInt("pos");
+
         //create view
-        View v= LayoutInflater.from(getActivity())
+        View viewer= LayoutInflater.from(getActivity())
                 .inflate(R.layout.fragment_edit_route,null);
 
-        setupEditTexts();
+        final EditText editName=(EditText)viewer.findViewById(R.id.editName);
+        final EditText editCity=(EditText)viewer.findViewById(R.id.editCity);
+        final EditText editHighway=(EditText)viewer.findViewById(R.id.editHighway);
 
+        String nameToEdit=getArguments().getString("name");
+        int cityToEdit=getArguments().getInt("city");
+        int highwayToEdit=getArguments().getInt("highway");
+
+        editName.setText(nameToEdit);
+        editCity.setText(""+cityToEdit);
+        editHighway.setText(""+highwayToEdit);
         //create button listeners
         DialogInterface.OnClickListener listener= new DialogInterface.OnClickListener() {
+
+
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 switch(which) {
                     case DialogInterface.BUTTON_POSITIVE:
-                        EditText editName=(EditText)getActivity().findViewById(R.id.editTextName);
-                        EditText editCity=(EditText)getActivity().findViewById(R.id.editTextCity);
-                        EditText editHighway=(EditText)getActivity().findViewById(R.id.editTextHighway);
 
                         String name=editName.getText().toString();
-                        int city=Integer.parseInt(editCity.getText().toString());
-                        int highway=Integer.parseInt(editHighway.getText().toString());
-                        if(name.equals("")||city<0||highway<0){break;}
-                        int pos=getArguments().getInt("pos");
-                        ((SelectRoute)getActivity()).routes.get(pos).setCityDistance(city);
-                        ((SelectRoute)getActivity()).routes.get(pos).setHighwayDistance(highway);
-                        ((SelectRoute)getActivity()).routes.get(pos).setRouteName(name);
+                        String city=editCity.getText().toString();
+                        String highway=editHighway.getText().toString();
 
+                        if(name.equals("")||city.equals("")||highway.equals("")){
+                            Toast.makeText(getActivity(),"Fields cannot be empty.",Toast.LENGTH_SHORT).show();
+                            break;
+                        }
+                        else if(Integer.parseInt(city)<0||Integer.parseInt(highway)<0) {
+                            Toast.makeText(getActivity(),"Values cannot be negative.",Toast.LENGTH_SHORT).show();
+                            break;
+                        }
+                        ((SelectRoute)getActivity()).changeRoute(pos,name,Integer.parseInt(city),Integer.parseInt(highway));
+                        ((SelectRoute)getActivity()).setUpSpinner();
                         break;
                     case DialogInterface.BUTTON_NEGATIVE:
 
                         break;
                 }
 
-
             }
         };
         //build alert dialog
         return new AlertDialog.Builder(getActivity())
                 .setTitle("Edit Route")
-                .setView(v)
+                .setView(viewer)
                 .setPositiveButton(android.R.string.ok,listener)
                 .setNegativeButton(android.R.string.cancel,listener)
                 .create();
-    }
-
-    private void setupEditTexts() {
-        String name=getArguments().getString("name");
-        int city=getArguments().getInt("city");
-        int highway=getArguments().getInt("highway");
-
-        EditText editName=(EditText)getActivity().findViewById(R.id.editTextName);
-        EditText editCity=(EditText)getActivity().findViewById(R.id.editTextCity);
-        EditText editHighway=(EditText)getActivity().findViewById(R.id.editTextHighway);
-
-        editName.setText(name);
-        editCity.setText(""+city);
-        editHighway.setText(""+highway);
     }
 }
