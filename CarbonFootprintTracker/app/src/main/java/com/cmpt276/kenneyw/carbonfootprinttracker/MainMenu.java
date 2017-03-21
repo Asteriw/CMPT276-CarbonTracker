e3556551f7f6e379c9c2ce9ad6f097da161c8ea0
kenneyw <kenneyw@sfu.ca>
2017-03-20 19:25:40 -0700

    The tip AlertDialog works

--- CarbonFootprintTracker/app/src/main/java/com/cmpt276/kenneyw/carbonfootprinttracker/MainMenu.java
+++ CarbonFootprintTracker/app/src/main/java/com/cmpt276/kenneyw/carbonfootprinttracker/MainMenu.java
@@ -1,11 +1,17 @@
 package com.cmpt276.kenneyw.carbonfootprinttracker;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -15,6 +21,60 @@ public class MainMenu extends AppCompatActivity {
         setContentView(R.layout.activity_main_menu);
         setupJourneyButtons();
         setupFootprintButtons();
+        setupTipButton();
+    }
+
+    private void setupTipButton() {
+        Button TipButton = (Button) findViewById(R.id.tiptextbutton);
+        TipButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tipMaker();
+            }
+        });
+    }
+
+    int tipCounter = 0;
+    String tipString = "";
+
+    private void tipMaker() {
+        LayoutInflater inflater = LayoutInflater.from(MainMenu.this);
+        View tipView =inflater.inflate(R.layout.activity_tip_dialog, null);
+
+        TextView tipText = (TextView) tipView.findViewById(R.id.tip_text);
+        tipText.setGravity(Gravity.CENTER);
+        tipText.setText(tipTextSelector());
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainMenu.this);
+        builder.setTitle("Tips!");
+        //alertDialog.setMessage("Here is a really long message.");
+        builder.setView(tipView);
+        builder.setPositiveButton("Next Tip", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                tipMaker();
+                Log.i("Dialog", "We got here once");
+            }
+        });
+        builder.setNegativeButton("Close Tips", null);
+        AlertDialog tipDialog = builder.create();
+        tipDialog.show();
+    }
+
+    private CharSequence tipTextSelector() {
+        if (tipCounter==0) {
+            tipString = "Tipperoni";
+            tipCounter++;
+            return tipString;
+        }
+        else if (tipCounter==1) {
+            tipString = "Just the tips";
+            tipCounter++;
+        }
+        else if (tipCounter>1) {
+            tipString = "No more Tips for you!";
+            tipCounter++;
+        }
+        return tipString;
     }
 
     private void setupFootprintButtons() {
