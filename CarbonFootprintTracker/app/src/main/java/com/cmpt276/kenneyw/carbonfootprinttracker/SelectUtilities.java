c7c7124e951d19318f40739445dee11a8fcc513b
Daniel Lee <dlee0528@hotmail.com>
2017-03-22 19:52:58 -0700

    SharedPref set up for Add Utility

--- CarbonFootprintTracker/app/src/main/java/com/cmpt276/kenneyw/carbonfootprinttracker/SelectUtilities.java
+++ CarbonFootprintTracker/app/src/main/java/com/cmpt276/kenneyw/carbonfootprinttracker/SelectUtilities.java
@@ -3,6 +3,7 @@ package com.cmpt276.kenneyw.carbonfootprinttracker;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -18,6 +19,16 @@ public class SelectUtilities extends AppCompatActivity {
 
     public static final int  SELECT_UTILITY= 1;
     private static final String TAG = "CarbonFootprintTracker";
+    private static final String SHAREDPREF_SET = "CarbonFootprintTrackerUtilities";
+    private static final String SHAREDPREF_ITEM_AMOUNTOFUTILITIES = "AmountOfUtilities";
+    private static final String NAME ="name";
+    private static final String GASTYPE="gasType";
+    private static final String AMOUNT="amount";
+
+    private static final String NUMPEOPLE="numofPeople";
+    private static final String STARTDATE="startDate";
+    private static final String ENDDATE="endDate";
+    private static final String EMISSION = "emission";
 
     ArrayList<Utility> utilities = new ArrayList<>();
     Intent intent;
@@ -26,9 +37,39 @@ public class SelectUtilities extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_utilities);
-
+        utilities=loadUtilities();
         setupList();
         setupButtons();
+    }
+
+    private ArrayList<Utility> loadUtilities() {
+        ArrayList<Utility> utils = new ArrayList<>();
+        SharedPreferences pref=getSharedPreferences(SHAREDPREF_SET,MODE_PRIVATE);
+        int utilityAmt=pref.getInt(SHAREDPREF_ITEM_AMOUNTOFUTILITIES,0);
+        for(int i=0;i<utilityAmt;i++) {
+            Utility newUtil=new Utility(pref.getString(i+NAME,""),
+                    pref.getString(i+GASTYPE,""),Double.longBitsToDouble(pref.getLong(i+AMOUNT,0)),pref.getInt(i+NUMPEOPLE,0),
+                    Double.longBitsToDouble(pref.getLong(i+EMISSION,0)),pref.getString(i+STARTDATE,""),pref.getString(i+ENDDATE,""));
+            utils.add(newUtil);
+        }
+        return utils;
+    }
+    private void saveUtilities() {
+        SharedPreferences pref=getSharedPreferences(SHAREDPREF_SET,MODE_PRIVATE);
+        SharedPreferences.Editor editor=pref.edit();
+        editor.clear();
+        int utilityAmt=utilities.size();
+        for(int i=0;i<utilityAmt;i++) {
+            editor.putString(i+NAME,utilities.get(i).getName());
+            editor.putString(i+GASTYPE,utilities.get(i).getGasType());
+            editor.putString(i+STARTDATE,utilities.get(i).getStartDate());
+            editor.putString(i+ENDDATE,utilities.get(i).getEndDate());
+            editor.putInt(i+NUMPEOPLE,utilities.get(i).getNumofPeople());
+            editor.putLong(i+EMISSION,Double.doubleToRawLongBits(utilities.get(i).getEmission()));
+            editor.putLong(i+AMOUNT,Double.doubleToRawLongBits(utilities.get(i).getAmount()));
+        }
+        editor.putInt(SHAREDPREF_ITEM_AMOUNTOFUTILITIES,utilityAmt);
+        editor.apply();
     }
 
     private void setupList() {
@@ -49,6 +90,7 @@ public class SelectUtilities extends AppCompatActivity {
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                saveUtilities();
                 finish();
             }
         });
@@ -88,6 +130,12 @@ public class SelectUtilities extends AppCompatActivity {
                 break;
         }
     }
+
+    @Override
+    public void onBackPressed() {
+        saveUtilities();
+    }
+
     public static Intent makeIntent(Context context) {
         return new Intent(context, SelectUtilities.class);
     }
