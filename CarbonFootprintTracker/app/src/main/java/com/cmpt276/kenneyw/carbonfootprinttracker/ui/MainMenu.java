d79cdf559456841b136de80f8c1e0d6eb1448d37
Daniel Lee <hla191@sfu.ca>
2017-04-04 20:18:41 -0700

    Added "about" screen and fade animation on the MainMenu screen

--- CarbonFootprintTracker/app/src/main/java/com/cmpt276/kenneyw/carbonfootprinttracker/ui/MainMenu.java
+++ CarbonFootprintTracker/app/src/main/java/com/cmpt276/kenneyw/carbonfootprinttracker/ui/MainMenu.java
@@ -5,12 +5,18 @@ package com.cmpt276.kenneyw.carbonfootprinttracker.ui;
  * Nothing too special.
  */
 
+
 import android.content.Context;
 import android.content.Intent;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.cmpt276.kenneyw.carbonfootprinttracker.R;
 
@@ -21,8 +27,14 @@ public class MainMenu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
         setupButtons();
+        setupAnimation();
     }
-
+    // Fade carlogo in the MainMenu Screen
+    private void setupAnimation(){
+        ImageView myImageView = (ImageView) findViewById(R.id.car_logo);
+        Animation carAnimation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade);
+        myImageView.startAnimation(carAnimation);
+    }
     private void setupButtons() {
         // directs to "SelectJourney" screen
         Button journey_button = (Button) findViewById(R.id.create_a_new_journey_button);
@@ -51,7 +63,18 @@ public class MainMenu extends AppCompatActivity {
                 startActivity(MainMenu2Utilities);
             }
         });
+        // directs to "About" screen
+        Button about_button = (Button) findViewById(R.id.about_btn);
+        about_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager manager = getSupportFragmentManager();
+                AboutFragment dialog = new AboutFragment();
+                dialog.show(manager, "About");
+            }
+        });
     }
+
 
     public static Intent makeIntent(Context context) {
         return new Intent(context, MainMenu.class);
