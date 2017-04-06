08ed97eaa6923b128db582c6d22695b3039e60c0
Paul <allanpaulw@gmail.com>
2017-04-05 23:23:44 -0700

    Notifications are up.
    TODO:
    -Read/Write from/to SharedPreferences
    -Detect time and notify
    -Conditional statements.

--- CarbonFootprintTracker/app/src/main/java/com/cmpt276/kenneyw/carbonfootprinttracker/ui/WelcomeScreen.java
+++ CarbonFootprintTracker/app/src/main/java/com/cmpt276/kenneyw/carbonfootprinttracker/ui/WelcomeScreen.java
@@ -5,10 +5,15 @@ package com.cmpt276.kenneyw.carbonfootprinttracker.ui;
  * Leads to main menu.
  */
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.LinearInterpolator;
@@ -25,6 +30,7 @@ public class WelcomeScreen extends AppCompatActivity {
         setContentView(R.layout.activity_welcome_screen);
         setupButton();
         setupAnimation();
+        showDefaultNotification();
     }
     private void setupAnimation() {
         ImageView car = (ImageView) findViewById(R.id.splashcar);
@@ -46,6 +52,49 @@ public class WelcomeScreen extends AppCompatActivity {
             }
         });
     }
+
+    public void showDefaultNotification() {
+        PendingIntent pi = PendingIntent.getActivity(this, 0, new Intent(this, MainMenu.class), 0);
+        Resources r = getResources();
+        Notification notification = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.car_notification)
+                .setContentTitle(r.getString(R.string.notification_title))
+                .setContentText(r.getString(R.string.notification_text))
+                .setContentIntent(pi)
+                .setAutoCancel(true)
+                .build();
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(0, notification);
+    }
+
+    public void showBillsNotification() {
+        PendingIntent pi = PendingIntent.getActivity(this, 0, new Intent(this, SelectUtilities.class), 0);
+        Resources r = getResources();
+        Notification notification = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.car_notification)
+                .setContentTitle(r.getString(R.string.notification_title))
+                .setContentText(String.format(getString(R.string.notification_text_bills), 1)) //TODO: Change this value
+                .setContentIntent(pi)
+                .setAutoCancel(true)
+                .build();
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(0, notification);
+    }
+
+    public void showJourneysNotification() {
+        PendingIntent pi = PendingIntent.getActivity(this, 0, new Intent(this, SelectJourney.class), 0);
+        Resources r = getResources();
+        Notification notification = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.car_notification)
+                .setContentTitle(r.getString(R.string.notification_title))
+                .setContentText(String.format(getString(R.string.notification_text_journeys_more_than_one), 1))//// TODO: CHANGE THIS VALUE and journeys >1
+                .setContentIntent(pi)
+                .setAutoCancel(true)
+                .build();
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(0, notification);
+    }
+
     public static Intent makeIntent(Context context) {
         return new Intent(context, WelcomeScreen.class);
     }
